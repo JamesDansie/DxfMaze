e1768967559f496dc80cbe6620f2184b545fc151
JamesDansie <jdansie8336@gmail.com>
2020-03-28 14:49:37 -0700

    starting to make nodes, saved dxf is broken

--- mazes.py
+++ mazes.py
@@ -6,6 +6,10 @@ class Maze:
         def __init__(self, position):
             self.Position = position
             self.Neighbors = [None, None, None, None]
+            # Neighbor[0] is up
+            # Neighbor[1] is right
+            # Neighbor[2] is down
+            # Neighbor[3] is left
 
     def __init__(self, input_file):
     
@@ -55,7 +59,16 @@ class Maze:
         lines_vertical = []
         lines_diagnoal = []
 
+        xmin = xmax = lines[0].dxf.start[0]
+        ymin = ymax = lines[0].dxf.start[0]
+
         for line in lines:
+            xmin = min(xmin, line.dxf.start[0], line.dxf.end[0])
+            xmax = max(xmax, line.dxf.start[0], line.dxf.end[0])
+            ymin = min(ymin, line.dxf.start[1], line.dxf.end[1])
+            ymax = max(ymax, line.dxf.start[1], line.dxf.end[1])
+
+            # Sorting lines in vertical, horizontal and diagonal
             if(_isHoriz(line.dxf.start, line.dxf.end)):
                 lines_horizontal.append(line)
             elif(_isVert(line.dxf.start, line.dxf.end)):
@@ -67,7 +80,72 @@ class Maze:
         print('there are ', len(lines_horizontal), 'horizontal lines')
         print('there are ', len(lines_vertical), 'vertical lines')
         print('there are ', len(lines_diagnoal), 'diagonal lines')
+        print('the min pt is ({},{})'.format(xmin, ymin))
+        print('the max pt is ({},{})'.format(xmax, ymax))
 
+        # Starting and ending points
+        # Eventually replace with user prompts 
+
+        xstart = 120
+        ystart = 230
+        xend = 256
+        yend = 128
+        self.start = None
+        self.end = None
+
+        # ******* Making Nodes/Graph **********
+
+        # Rounding to the nearest ft, this will make sure the start and end node positions are included.
+        xmin_ft = xcounter_ft = round(xmin/12)
+        ymin_ft = ycounter_ft = round(ymin/12)
+        xmax_ft = round(xmax/12)
+        ymax_ft = round(ymax/12)
+        node_increment_ft = 1
+        nodes = []
+
+        print('xcounter: ',xcounter_ft,' xmax ',xmax_ft)
+
+        while ycounter_ft < ymax_ft:
+        
+            xcounter_ft = xmin_ft
+            nodes_row = []
+            prev = None
+
+            while xcounter_ft < xmax_ft:                
+                curr = Maze.Node((xcounter_ft, ycounter_ft))
+                print(curr.Position)
+                dxf_msp.add_circle(curr.Position, 0.3)
+                # curr adds the previous node to the left
+                
+                if(
+                    prev != None and 
+                    _intersect_lines(lines_vertical, curr.Position, prev.Position) and
+                    _intersect_lines(lines_diagnoal, curr.Position, prev.Position)
+                    ):
+                    # the previous node to the left adds the current node to the right if there's no walls in the way
+                    curr.Neighbors[3] = prev
+                    prev.Neighbors[1] = curr
+                    dxf_msp.add_line(curr.Position, prev.Position)
+
+                # if(ycounter_ft != ymin_ft):
+
+
+                if(xcounter_ft == xstart and ycounter_ft == ystart):
+                    #found start node
+                    self.start = curr
+                elif(xcounter_ft == xend and ycounter_ft == yend):
+                    #found end node
+                    self.end = curr
+
+                nodes_row.append(curr)
+                prev = curr
+                xcounter_ft += node_increment_ft
+
+            nodes.append(nodes_row)
+            ycounter_ft += node_increment_ft
+
+        dxf_doc.save()
+        dxf_doc.saveas('nodes.dxf')
         # ***** testing zone below *****
 
         # Testing for intersection and line direction
@@ -101,6 +179,21 @@ class Maze:
             (test_node1.Position[0], test_node1.Position[1]),
             (test_node2.Position[0], test_node2.Position[1])
         ))
+
+        print('intersect horizontal lines? ', _intersect_lines(
+            lines_horizontal,
+            test_node1.Position,
+            test_node2.Position
+        ))
+
+def _intersect_lines(lines, pos1, pos2):
+    for line in lines:
+        if(_intersect(
+            (line.dxf.start[0], line.dxf.start[1]),
+            (line.dxf.end[0], line.dxf.end[1]),
+            pos1, pos2
+        )): return True
+    return False
 
 def _print_entity(e):
     print("LINE on layer: %s\n" % e.dxf.layer)
