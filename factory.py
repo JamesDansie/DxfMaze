ae548ca1f7818ac8012d18de2683bd8c68968b03
JamesDansie <jdansie8336@gmail.com>
2020-04-09 09:14:49 -0700

    depth first is working

--- factory.py
+++ factory.py
@@ -7,20 +7,15 @@ class SolverFactory:
         self.Choices = ["breadthfirst","depthfirst","dijkstra", "astar","leftturn"]
 
     def createsolver(self, type):
-        # Stuff to be implemented. Currently place holders
-        
-        # if type == "leftturn":
-        #     import leftturn
-        #     return ["Left turn only", leftturn.solve]
-        # elif type == "depthfirst":
-        #     import depthfirst
-        #     return ["Depth first search", depthfirst.solve]
+        if type == "depthfirst":
+            import depthfirst
+            return ["Depth first search", depthfirst.solve]
         # elif type == "dijkstra":
         #     import dijkstra
         #     return ["Dijkstra's Algorithm", dijkstra.solve]
         # elif type == "astar":
         #     import astar
         #     return ["A-star Search", astar.solve]
-        # else:
+        else:
             import breadthfirst
             return ["Breadth first search", breadthfirst.solve]
